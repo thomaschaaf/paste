9943dd350436c5574eb2f46d8779d79715c1b2a9
Si Taggart <me@simontaggart.com>
2021-07-27 11:24:50 -0700

    chore: checkpoint

--- .storybook/main.js
+++ .storybook/main.js
@@ -45,6 +45,8 @@ module.exports = {
     config.resolve.plugins =
       config.resolve.plugins == null ? customPlugins : [...config.resolve.plugins, ...customPlugins];
 
+    config.resolve.symlinks = true;
+
     return config;
   },
 };
